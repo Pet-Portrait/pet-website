8ac51272045a1e04cbde6c8e3d3b1a1dc5b99e06
Jakub Ciepły <jakubc01@gmail.com>
2022-11-08 22:13:16 +0100

    PET-70 Limit X movement

--- src/hooks/useGyroscopicParallax.ts
+++ src/hooks/useGyroscopicParallax.ts
@@ -2,10 +2,11 @@ import { RefObject, useEffect, useRef } from 'react';
 
 import useRequestAnimationFrame from './useRequestAnimationFrame';
 
-const SPEED_FACTOR_X = 0.2;
-const SPEED_FACTOR_Y = 0.3;
+const SPEED_FACTOR_X = 0.1;
+const SPEED_FACTOR_Y = 0.1;
 
 const MOVE_Y_LIMIT = 2;
+const MOVE_X_LIMIT = 5;
 
 const useGyroscopicParallax = <RefType extends RefObject<HTMLElement>>(ref: RefType) => {
   const initialPosition = useRef<{ beta: number; gamma: number } | null>(null);
@@ -24,8 +25,10 @@ const useGyroscopicParallax = <RefType extends RefObject<HTMLElement>>(ref: RefT
         const dBeta = beta - initialPosition.current.beta;
         const dGamma = gamma - initialPosition.current.gamma;
 
-        const moveX = dGamma * SPEED_FACTOR_X;
-        const moveY = Math.min(Math.max(dBeta * SPEED_FACTOR_Y, MOVE_Y_LIMIT * -1), MOVE_Y_LIMIT);
+        const getNumberInRange = (number: number, min: number, max: number) =>
+          Math.min(Math.max(number, min), max);
+        const moveX = getNumberInRange(dGamma * SPEED_FACTOR_X, MOVE_X_LIMIT * -1, MOVE_X_LIMIT);
+        const moveY = getNumberInRange(dBeta * SPEED_FACTOR_Y, MOVE_Y_LIMIT * -1, MOVE_Y_LIMIT);
 
         ref.current.style.transform = `translate3d(${moveX}px, ${moveY}px, 0)`;
       });
