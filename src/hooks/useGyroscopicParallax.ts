7cc2133f2e75df71888d98be6068770a0fe29389
Jakub Ciepły <jakubc01@gmail.com>
2022-11-08 21:23:26 +0100

    PET-70 Refactor code

--- src/hooks/useGyroscopicParallax.ts
+++ src/hooks/useGyroscopicParallax.ts
@@ -1,54 +1,33 @@
-import { RefObject, useEffect } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 
 const SPEED_FACTOR_X = 0.2;
 const SPEED_FACTOR_Y = 0.3;
 
-let initialPosition: { beta: number; gamma: number } | null = null;
-let prevPosition: { beta: number; gamma: number } | null = null;
-
-let rafTimeout: number | null = null;
+const MOVE_Y_LIMIT = 2;
 
 const useGyroscopicParallax = <RefType extends RefObject<HTMLElement>>(ref: RefType) => {
+  const initialPosition = useRef<{ beta: number; gamma: number } | null>(null);
+  const rafTimeout = useRef<number | null>(null);
+
   useEffect(() => {
     const animateImage = (event: DeviceOrientationEvent) => {
-      const { beta: _beta, gamma: _gamma } = event;
-      const beta = _beta;
-      const gamma = _gamma;
+      const { beta, gamma } = event;
 
-      if (rafTimeout) {
-        window.cancelAnimationFrame(rafTimeout);
+      if (rafTimeout.current) {
+        window.cancelAnimationFrame(rafTimeout.current);
       }
 
-      rafTimeout = window.requestAnimationFrame(() => {
+      rafTimeout.current = window.requestAnimationFrame(() => {
         if (beta === null || gamma === null || !ref.current) return;
-        if (!initialPosition) {
-          initialPosition = { beta, gamma };
-          prevPosition = { beta, gamma };
+        if (!initialPosition.current) {
+          initialPosition.current = { beta, gamma };
         }
-        // if (prevPosition) {
-        //   if (Math.abs(beta - prevPosition.beta) > 3) {
-        //     beta = prevPosition.beta + 1 * Math.sign(beta - prevPosition.beta);
-        //   }
-        //   if (Math.abs(gamma - prevPosition.gamma) > 3) {
-        //     gamma = prevPosition.gamma + 1 * Math.sign(gamma - prevPosition.gamma);
-        //   }
-        // }
-        // prevPosition = { beta, gamma };
 
-        const dBeta = beta - initialPosition.beta;
-        const dGamma = gamma - initialPosition.gamma;
+        const dBeta = beta - initialPosition.current.beta;
+        const dGamma = gamma - initialPosition.current.gamma;
 
         const moveX = dGamma * SPEED_FACTOR_X;
-        const moveY = Math.min(Math.max(dBeta * SPEED_FACTOR_Y, -5), 5);
-        // eslint-disable-next-line no-console
-        console.log({
-          prevPosition,
-          moveX,
-          moveY,
-          beta,
-          gamma,
-          initialPosition: initialPosition,
-        });
+        const moveY = Math.min(Math.max(dBeta * SPEED_FACTOR_Y, MOVE_Y_LIMIT * -1), MOVE_Y_LIMIT);
 
         ref.current.style.transform = `translate3d(${moveX}px, ${moveY}px, 0)`;
       });
