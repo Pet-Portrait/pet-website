f7146f82c56630fd1642142e0e43c621da332dcf
Jakub Ciepły <jakubc01@gmail.com>
2022-11-11 03:13:04 +0100

    PET-81 Update design on mobile

--- src/components/homepage/HeroImage/HeroImage.tsx
+++ src/components/homepage/HeroImage/HeroImage.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useRef, useState } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Button from 'components/shared/Button/Button';
 import useGyroscopicParallax from 'hooks/useGyroscopicParallax';
 import useIntersectionObserver from 'hooks/useIntersectionObserver';
 import useParallax from 'hooks/useParallax';
-import logo from 'images/logo-black.svg';
 
 import ImageFactory from './ImageFactory';
+import LogoIcon from './LogoIcon';
 
 import * as classes from './HeroImage.module.scss';
 
@@ -25,19 +26,29 @@ const HeroImage: FC = () => {
 
   return (
     <section className={classes.root}>
-      <div>
-        <img alt="Logo pet portrait" className={classes.logo} src={logo} />
-        <h1>Rzuć groszem dla zwierzaków poprzez licytację sztuki!</h1>
-        <p className={classes.text}>
-          Myślę, że warto podać zwycięski skład Kaiserslautern z meczu z Bayernem w sezonie 97/98:
-          Renke, Kadlec, H. Koch (31. Hristov), Kuka, Marschall, Ratinho (75. Reich), Roos,
-          Schjönberg, Schäfer, Sforza, M. Wagner. I jeszcze Ballack.
-        </p>
-        <Button className={classes.button} to="/licytacje">
-          Kup bilet na żyletę
-        </Button>
+      <StaticImage
+        alt=""
+        className={classes.background}
+        layout="constrained"
+        objectPosition="left"
+        placeholder="blurred"
+        src="../../../images/backgrounds/hero.png"
+      />
+      <div className={classes.grid}>
+        <div className={classes.content}>
+          <LogoIcon />
+          <h1 className={classes.heading}>Rzuć groszem dla zwierzaków poprzez licytację sztuki!</h1>
+          <p className={classes.text}>
+            Myślę, że warto podać zwycięski skład Kaiserslautern z meczu z Bayernem w sezonie 97/98:
+            Renke, Kadlec, H. Koch (31. Hristov), Kuka, Marschall, Ratinho (75. Reich), Roos,
+            Schjönberg, Schäfer, Sforza, M. Wagner. I jeszcze Ballack.
+          </p>
+          <Button className={classes.button} to="/licytacje">
+            Kup bilet na żyletę
+          </Button>
+        </div>
+        <ImageFactory ref={imageRef} />
       </div>
-      <ImageFactory ref={imageRef} />
     </section>
   );
 };
