a80c11eda8c0e18469107d22976d801410afba26
Jakub Ciepły <jakubc01@gmail.com>
2022-11-16 19:52:16 +0100

    PET-66 Add entry animation

--- src/components/shared/Menu/Menu.tsx
+++ src/components/shared/Menu/Menu.tsx
@@ -1,18 +1,32 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 
 import Hamburger from './Hamburger/Hamburger';
 
 import * as classes from './Menu.module.scss';
 
+const TIME_TO_APPEAR_IN_MS = 1000;
+
 const Menu: FC = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleClick = () => {
     setIsActive((prevIsActive) => !prevIsActive);
   };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(true);
+    }, TIME_TO_APPEAR_IN_MS);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
-    <div className={classes.root}>
+    <div className={`${classes.root} ${isVisible ? classes.isVisible : ''}`}>
       <ul className={`${classes.list} ${isActive ? classes.listActive : ''}`}>
         <li className={classes.listItem}>
           <Link className={classes.link} to="/">
