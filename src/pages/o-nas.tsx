13f01cc024492950fae262c4ac7c24ca9c93eade
Jakub Ciepły <jakubc01@gmail.com>
2022-11-20 20:00:22 +0100

    NO-TASK Add SEO to about us page

--- src/pages/o-nas.tsx
+++ src/pages/o-nas.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable react/prop-types */
+
 import React, { FC } from 'react';
+import { HeadFC } from 'gatsby';
 
 import AboutUs from 'components/aboutUs/AboutUs';
+import Seo from 'components/shared/Seo/Seo';
 
 const AboutUsPage: FC = () => <AboutUs />;
 
 export default AboutUsPage;
+
+export const Head: HeadFC = ({ location }) => (
+  <Seo pathname={location.pathname} title="pet portrait | o nas" />
+);
